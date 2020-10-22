835da3eb9a1417feb004502471ba0889ea277792
Ryan Adams <ryan.p.adams@gmail.com>
2020-10-22 13:35:35 -0400

    Switched over to a class rather than a namedtuple for Shape2D.  Added logic for storing and showing labels.

--- varmint/shape.py
+++ varmint/shape.py
@@ -3,23 +3,25 @@ import jax.numpy         as np
 import numpy             as onp
 import matplotlib.pyplot as plt
 
-from collections import namedtuple
+from exceptions import DimensionError
 
 import bsplines
 
-# Create a numpy dtype with labels.
-ctrl2d = onp.dtype([('loc', onp.float64, (2,)), ('label', onp.str)])
+class Patch2D:
+  ''' Class for individual patches in two dimensions.
+  '''
+  def __init__(self, ctrl, xknots, yknots, deg, labels=None):
+    self.ctrl   = ctrl
+    self.xknots = xknots
+    self.yknots = yknots
+    self.deg    = deg
 
-# Create a simple structure for patches.
-Patch2D = namedtuple(
-  'Patch2D',
-  [
-    'ctrl',
-    'xknots',
-    'yknots',
-    'deg',
-  ],
-)
+    if labels is None:
+      self.labels = onp.zeros((ctrl.shape[0], ctrl.shape[1]), dtype='<U256')
+    else:
+      if labels.shape != ctrl.shape[:-1]:
+        raise DimensionError('The labels must have shape %d x %d.' % (
+          ctrl.shape[0], ctrl.shape[1]))
 
 class Shape2D:
   ''' Class for managing collections of 2D patches
@@ -36,7 +38,11 @@ class Shape2D:
 
     uu = np.linspace(0, 1, 1002)[1:-1]
 
+    rendered_labels = set()
+
     for patch in self.patches:
+
+      # Plot vertical lines.
       for jj in range(patch.ctrl.shape[1]):
         xx = bsplines.bspline1d(
           uu,
@@ -45,6 +51,8 @@ class Shape2D:
           patch.deg,
         )
         ax.plot(xx[:,0], xx[:,1], 'k-')
+
+      # Plot horizontal lines.
       for ii in range(patch.ctrl.shape[0]):
         yy = bsplines.bspline1d(
           uu,
@@ -53,6 +61,23 @@ class Shape2D:
           patch.deg,
         )
         ax.plot(yy[:,0], yy[:,1], 'k-')
+
+      # Plot the control points themselves.
+      ax.plot(patch.ctrl[:,:,0].ravel(), patch.ctrl[:,:,1].ravel(), 'k.')
+
+      # Plot labels.
+      label_r, label_c = onp.where(patch.labels)
+      for ii in range(len(label_r)):
+        row = label_r[ii]
+        col = label_c[ii]
+        text = patch.labels[row,col]
+        if text not in rendered_labels:
+          rendered_labels.add(text)
+        else:
+          continue
+        ax.annotate(text, patch.ctrl[row,col,:])
+
+
     if filename is None:
       plt.show()
     else:
@@ -62,16 +87,14 @@ class Shape2D:
 def main():
 
   # Create a rectangle.
-  r1_locs = bsplines.mesh(np.arange(10), np.arange(5))
-  r1_ctrl = onp.zeros(r1_locs.shape[:-1], dtype=ctrl2d)
-  r1_ctrl['loc'] = onp.array(r1_locs)
-
-  print(r1_ctrl)
-
   r1_deg    = 4
+  r1_ctrl   = bsplines.mesh(np.arange(10), np.arange(5))
   r1_xknots = bsplines.default_knots(r1_deg, r1_ctrl.shape[0])
   r1_yknots = bsplines.default_knots(r1_deg, r1_ctrl.shape[1])
   r1_patch  = Patch2D(r1_ctrl, r1_xknots, r1_yknots, r1_deg)
+  r1_patch.labels[3:7,0]  = ['A', 'B', 'C', 'D']
+  r1_patch.labels[:3,-1]  = ['E', 'F', 'G']
+  r1_patch.labels[-3:,-1] = ['H', 'I', 'J']
 
   # Create another rectangle.
   r2_deg    = 3
@@ -79,6 +102,7 @@ def main():
   r2_xknots = bsplines.default_knots(r2_deg, r2_ctrl.shape[0])
   r2_yknots = bsplines.default_knots(r2_deg, r2_ctrl.shape[1])
   r2_patch  = Patch2D(r2_ctrl, r2_xknots, r2_yknots, r2_deg)
+  r2_patch.labels[:,-1] = ['A', 'B', 'C', 'D']
 
   # Bend a u-shaped thing around the top.
   u1_deg  = 2
@@ -96,6 +120,8 @@ def main():
   u1_xknots = bsplines.default_knots(u1_deg, u1_ctrl.shape[0])
   u1_yknots = bsplines.default_knots(u1_deg, u1_ctrl.shape[1])
   u1_patch  = Patch2D(u1_ctrl, u1_xknots, u1_yknots, u1_deg)
+  u1_patch.labels[:,0]  = ['E', 'F', 'G']
+  u1_patch.labels[:,-1] = ['H', 'I', 'J']
 
   shape = Shape2D(r1_patch, r2_patch, u1_patch)
   shape.render()
