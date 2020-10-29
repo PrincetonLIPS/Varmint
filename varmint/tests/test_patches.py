42d09024e27442cffd639d2b2df672badc9ee960
Ryan Adams <ryan.p.adams@gmail.com>
2020-10-29 12:51:02 -0400

    Refactored patch tests to have more appropriate structure.

--- varmint/tests/test_patches.py
+++ varmint/tests/test_patches.py
@@ -9,82 +9,110 @@ import bsplines
 from patch      import Patch2D
 from exceptions import LabelError
 
-class TestPatch2D(ut.TestCase):
+class Test_Patch2D_NoLabels(ut.TestCase):
+  ''' Test basic functionality with no labels. '''
 
-  def test_1(self):
-    ''' Test basic functionality with no labels. '''
-
+  def setUp(self):
     deg    = 4
     ctrl   = bsplines.mesh(np.arange(10), np.arange(5))
     xknots = bsplines.default_knots(deg, ctrl.shape[0])
     yknots = bsplines.default_knots(deg, ctrl.shape[1])
-    patch  = Patch2D(ctrl, xknots, yknots, deg)
 
-    self.assertEqual(patch.ctrl.shape, (10,5,2))
-    self.assertEqual(patch.ctrl[0,0,0], 0)
-    self.assertEqual(patch.ctrl[-1,-1,1], 4)
-    self.assertEqual(patch.xknots[0], 0)
-    self.assertEqual(patch.xknots[-1], 1)
-    self.assertEqual(patch.yknots[0], 0)
-    self.assertEqual(patch.yknots[-1], 1)
-    self.assertEqual(patch.deg, 4)
-    self.assertFalse(patch.has_label('foo'))
-    self.assertTrue(np.all(patch.labels == ''))
+    self.patch = Patch2D(ctrl, xknots, yknots, deg)
+
+  def test_patch_shape(self):
+    self.assertEqual(self.patch.ctrl.shape, (10,5,2))
+
+  def test_patch_values(self):
+    self.assertEqual(self.patch.ctrl[0,0,0], 0)
+    self.assertEqual(self.patch.ctrl[-1,-1,1], 4)
+
+  def test_patch_knots(self):
+    self.assertEqual(self.patch.xknots[0], 0)
+    self.assertEqual(self.patch.xknots[-1], 1)
+    self.assertEqual(self.patch.yknots[0], 0)
+    self.assertEqual(self.patch.yknots[-1], 1)
+
+  def test_patch_deg(self):
+    self.assertEqual(self.patch.deg, 4)
+
+  def test_patch_no_labels(self):
+    self.assertTrue(np.all(self.patch.labels == ''))
+    self.assertFalse(self.patch.has_label('foo'))
+
+  def test_patch_label_errors(self):
+    with self.assertRaises(LabelError):
+      self.patch.label2idx('foo')
 
     with self.assertRaises(LabelError):
-      patch.label2idx('foo')
+      self.patch.label2ctrl('foo')
+
+  def test_patch_fixed_error(self):
+    with self.assertRaises(LabelError):
+      self.patch.has_label('FIXED')
 
     with self.assertRaises(LabelError):
-      patch.label2ctrl('foo')
+      self.patch.label2idx('FIXED')
 
     with self.assertRaises(LabelError):
-      patch.has_label('FIXED')
+      self.patch.label2ctrl('FIXED')
 
-    with self.assertRaises(LabelError):
-      patch.label2idx('FIXED')
+class Test_Patch2D_Labels(ut.TestCase):
+  ''' Test basic label functionality. '''
 
-    with self.assertRaises(LabelError):
-      patch.label2ctrl('FIXED')
-
-  def test_2(self):
-    ''' Test basic label functionality. '''
-
+  def setUp(self):
     deg    = 2
     ctrl   = bsplines.mesh(np.arange(10), np.arange(4))
     xknots = bsplines.default_knots(deg, ctrl.shape[0])
     yknots = bsplines.default_knots(deg, ctrl.shape[1])
-    patch  = Patch2D(ctrl, xknots, yknots, deg)
-    patch.labels[0,0] = '00'
-    patch.labels[-1,:] = ['A', 'B', 'C', 'D']
 
-    self.assertEqual(patch.ctrl.shape, (10,4,2))
-    self.assertEqual(patch.ctrl[0,0,0], 0)
-    self.assertEqual(patch.ctrl[-1,-1,1], 3)
-    self.assertEqual(patch.xknots[0], 0)
-    self.assertEqual(patch.xknots[-1], 1)
-    self.assertEqual(patch.yknots[0], 0)
-    self.assertEqual(patch.yknots[-1], 1)
-    self.assertEqual(patch.deg, 2)
-    self.assertFalse(patch.has_label('foo'))
-    self.assertTrue(patch.has_label('D'))
-    self.assertFalse(np.all(patch.labels == ''))
-    self.assertEqual(patch.labels[-1,0], 'A')
-    self.assertEqual(patch.label2idx('C'), (9,2))
+    self.patch  = Patch2D(ctrl, xknots, yknots, deg)
 
-    nptest.assert_array_equal(patch.label2ctrl('B'),
+    self.patch.labels[0,0] = '00'
+    self.patch.labels[-1,:] = ['A', 'B', 'C', 'D']
+
+  def test_patch_shape(self):
+    self.assertEqual(self.patch.ctrl.shape, (10,4,2))
+
+  def test_patch_values(self):
+    self.assertEqual(self.patch.ctrl[0,0,0], 0)
+    self.assertEqual(self.patch.ctrl[-1,-1,1], 3)
+
+  def test_patch_knots(self):
+    self.assertEqual(self.patch.xknots[0], 0)
+    self.assertEqual(self.patch.xknots[-1], 1)
+    self.assertEqual(self.patch.yknots[0], 0)
+    self.assertEqual(self.patch.yknots[-1], 1)
+
+  def test_patch_deg(self):
+    self.assertEqual(self.patch.deg, 2)
+
+  def test_patch_absent_labels(self):
+    self.assertFalse(self.patch.has_label('foo'))
+
+    with self.assertRaises(LabelError):
+      self.patch.label2idx('foo')
+
+    with self.assertRaises(LabelError):
+      self.patch.label2ctrl('foo')
+
+  def test_patch_has_labels(self):
+    self.assertFalse(np.all(self.patch.labels == ''))
+
+  def test_patch_present_label(self):
+    self.assertTrue(self.patch.has_label('D'))
+    self.assertEqual(self.patch.labels[-1,0], 'A')
+    self.assertEqual(self.patch.label2idx('C'), (9,2))
+
+    nptest.assert_array_equal(self.patch.label2ctrl('B'),
                               np.array([9.0, 1.0]))
 
+  def test_patch_fixed_error(self):
     with self.assertRaises(LabelError):
-      patch.label2idx('foo')
+      self.patch.has_label('FIXED')
 
     with self.assertRaises(LabelError):
-      patch.label2ctrl('foo')
+      self.patch.label2idx('FIXED')
 
     with self.assertRaises(LabelError):
-      patch.has_label('FIXED')
-
-    with self.assertRaises(LabelError):
-      patch.label2idx('FIXED')
-
-    with self.assertRaises(LabelError):
-      patch.label2ctrl('FIXED')
+      self.patch.label2ctrl('FIXED')
