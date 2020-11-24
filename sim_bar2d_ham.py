6937edfdd10e49f6ce59a0d8249bedddbeec0bb2
Ryan Adams <ryan.p.adams@gmail.com>
2020-11-24 17:10:33 -0500

    Trying the discretized Hamiltonian approach.

---
+++ sim_bar2d_ham.py
@@ -0,0 +1,98 @@
+import time
+import jax
+import jax.numpy      as np
+import numpy          as onp
+import scipy.optimize as spopt
+import jax.scipy.optimize as jopt
+
+from varmint.patch2d      import Patch2D
+from varmint.shape2d      import Shape2D
+from varmint.materials    import Material, SiliconeRubber
+from varmint.constitutive import NeoHookean2D
+from varmint.bsplines     import mesh, default_knots
+from varmint.lagrangian   import generate_lagrangian
+from varmint.discretize   import discretize_hamiltonian
+
+class WigglyMat(Material):
+  _E = 0.001
+  _nu = 0.48
+  _density = 1.0
+
+mat = NeoHookean2D(WigglyMat)
+
+# Length units are centimeters.
+length    = 50
+height    = 2
+num_xctrl = 10
+num_yctrl = 5
+ctrl      = mesh(np.linspace(0, length, num_xctrl),
+                 np.linspace(0, height, num_yctrl))
+
+# Make the patch.
+spline_deg = 3
+quad_deg   = 10
+xknots     = default_knots(spline_deg, num_xctrl)
+yknots     = default_knots(spline_deg, num_yctrl)
+labels = onp.zeros((num_xctrl, num_yctrl), dtype='<U256')
+labels[0,:] = ['A', 'B', 'C', 'D', 'E']
+fixed = {
+  'A': ctrl[0,0,:],
+  'B': ctrl[0,1,:],
+  'C': ctrl[0,2,:],
+  'D': ctrl[0,3,:],
+  'E': ctrl[0,4,:],
+}
+patch = Patch2D(
+  xknots,
+  yknots,
+  spline_deg,
+  mat,
+  quad_deg,
+  labels=labels,
+  fixed=fixed
+)
+shape = Shape2D(patch)
+
+# Reference configuration and initial deformation/velocity.
+ref_ctrl = [ctrl]
+def_ctrl = [ctrl.copy()]
+def_vels = [np.zeros_like(ctrl)]
+q, qdot  = shape.flatten(def_ctrl, def_vels)
+
+# Get the Lagrangian and then convert to discrete Euler-Lagrange.
+lagrangian = generate_lagrangian(shape, ref_ctrl)
+Ld_dq1, Ld_dq2 = discretize_hamiltonian(lagrangian)
+Ld_dq1_jac = jax.jit(jax.jacfwd(Ld_dq1, argnums=1))
+
+dt = 0.01
+t  = dt
+T  = 1.0
+
+# Initial locations and momenta.
+QQ = [ q.copy() ]
+PP = [ np.zeros_like(q) ]
+TT = [ 0.0 ]
+
+while TT[-1] < T:
+  print('\nt: %0.4f' % (TT[-1]))
+
+  t0 = time.time()
+
+  # Solve for the next q.
+  res = spopt.least_squares(
+    lambda q_1: PP[-1] + Ld_dq1(QQ[-1], q_1, dt), # should be zero
+    QQ[-1], # initialize at current location
+    lambda q_1: Ld_dq1_jac(QQ[-1], q_1, dt), # Jacobian
+    method='lm',
+  )
+  t1 = time.time()
+  print('\t%0.3f sec' % (t1-t0))
+  QQ.append(res.x)
+
+  # Get the new momentum.
+  PP.append( Ld_dq2(QQ[-2], QQ[-1], dt) )
+  TT.append(TT[-1]+dt)
+
+ctrl_seq = list(map(lambda qv: shape.unflatten(qv[0], qv[1])[0], zip(QQ,PP)))
+
+shape.create_movie(ctrl_seq, 'bar2d_ham.mp4', labels=True)
--- varmint/discretize.py
+++ varmint/discretize.py
@@ -14,3 +14,15 @@ def discretize_eulag(L):
     return grad_Ld_q1(q2, q3, t3-t2) + grad_Ld_q2(q1, q2, t2-t1)
 
   return DEL
+
+def discretize_hamiltonian(L):
+
+  def Ld(q1, q2, dt):
+    q    = (q1 + q2) / 2
+    qdot = (q2 - q1) / dt
+    return L(q, qdot)
+
+  grad_Ld_q1 = jax.jit(jax.grad(Ld, argnums=0))
+  grad_Ld_q2 = jax.jit(jax.grad(Ld, argnums=1))
+
+  return grad_Ld_q1, grad_Ld_q2
