bc267cf205d05cf2a7a81ef3499f085f7c435ac0
Ryan Adams <ryan.p.adams@gmail.com>
2020-12-15 16:00:14 -0500

    Starting to think about differentiating the integrator.

--- cell3.py
+++ cell3.py
@@ -12,7 +12,7 @@ from varmint.materials    import Material, SiliconeRubber
 from varmint.constitutive import NeoHookean2D
 from varmint.bsplines     import default_knots
 from varmint.lagrangian   import generate_lagrangian_structured
-from varmint.discretize   import discretize_hamiltonian
+from varmint.discretize   import get_hamiltonian_stepper
 from varmint.levmar       import get_lmfunc
 from varmint.cellular2d   import match_labels, generate_quad_lattice
 
@@ -66,14 +66,7 @@ q, qdot  = shape.get_flatten_fn()(def_ctrl, def_vels)
 
 lagrangian = generate_lagrangian_structured(shape, ref_ctrl)
 
-Ld_dq1, Ld_dq2 = discretize_hamiltonian(lagrangian)
-Ld_dq1_jac = jax.jit(jax.jacfwd(Ld_dq1, argnums=1))
-
-@jax.jit
-def residual_fun(q1, args):
-  q0, p0, dt = args
-  return p0 + Ld_dq1(q0, q1, dt)
-optfun = get_lmfunc(residual_fun, maxiters=200)
+stepper = get_hamiltonian_stepper(lagrangian)
 
 dt = 0.01
 t  = dt
@@ -91,27 +84,18 @@ while TT[-1] < T:
   print('\nt: %0.4f' % (TT[-1]))
 
   t0 = time.time()
-  if True:
-    res = spopt.least_squares(
-      lambda q_1: PP[-1] + Ld_dq1(QQ[-1], q_1, dt), # should be zero
-      QQ[-1], # initialize at current location
-      lambda q_1: Ld_dq1_jac(QQ[-1], q_1, dt), # Jacobian
-      method='lm',
-    )
-  else:
-    res = optfun(QQ[-1], (QQ[-1], PP[-1], dt))
+  new_q, new_p = stepper(QQ[-1], PP[-1], dt)
 
-  QQ.append(res.x)#.block_until_ready())
+  QQ.append(new_q)
+  PP.append(new_p)
 
   ctrl_seq.append( unflatten(QQ[-1], PP[-1])[0] )
 
-  # Get the new momentum.
-  PP.append( Ld_dq2(QQ[-2], QQ[-1], dt))#.block_until_ready() )
   TT.append(TT[-1]+dt)
 
   t1 = time.time()
-  print('\t%d' % (res.nfev))
-  print('\t%d' % (res.njev))
+  #print('\t%d' % (res.nfev))
+  #print('\t%d' % (res.njev))
   print('\t%0.3f sec' % (t1-t0))
 
 shape.create_movie(ctrl_seq, 'cell.mp4', labels=False)
--- varmint/discretize.py
+++ varmint/discretize.py
@@ -1,4 +1,7 @@
 import jax
+import jax.numpy as np
+import jax.numpy.linalg as npla
+import scipy.optimize as spopt
 
 def discretize_eulag(L):
 
@@ -26,3 +29,55 @@ def discretize_hamiltonian(L):
   grad_Ld_q2 = jax.jit(jax.grad(Ld, argnums=1))
 
   return grad_Ld_q1, grad_Ld_q2
+
+def get_hamiltonian_stepper(L):
+
+  Ld_q1, Ld_q2 = discretize_hamiltonian(L)
+  Ld_q1_jac = jax.jit(jax.jacfwd(Ld_q1, argnums=1))
+
+  # TODO: need to figure this out for time dependence.
+  @jax.custom_jvp
+  def step_q(q, p, dt):
+
+    res = spopt.least_squares(
+      lambda q_1: p + Ld_q1(q, q_1, dt),
+      q,
+      lambda q_1: Ld_q1_jac(q, q_1, dt),
+      method='lm',
+    )
+    # TODO: handle errors.
+
+    return res.x
+
+  @step_q.defjvp
+  def step_q_jvp(primals, tangents):
+    q, p, dt = primals
+    q_tan, p_tan, dt_tan = tangents
+
+    # Can I do this with the above version?
+    res = spopt.least_squares(
+      lambda q_1: p + Ld_q1(q, q_1, dt),
+      q,
+      lambda q_1: Ld_q1_jac(q, q_1, dt),
+      method='lm',
+    )
+    # TODO: handle errors.
+
+    new_q = res.x
+    jac   = res.jac # verify
+
+    old_q_tan = jvp(lambda old_q: Ld_q1(old_q, new_q, dt), (q,), q_tan)
+
+    new_q_tan = - npla.solve(jac, old_q_tan + p_tan)
+
+    return new_q, new_q_tan
+
+  def step_p(q1, q2, dt):
+    return Ld_q2(q1, q2, dt)
+
+  def stepper(q, p, dt):
+    new_q = step_q(q, p, dt)
+    new_p = step_p(q, new_q, dt)
+    return new_q, new_p
+
+  return stepper
