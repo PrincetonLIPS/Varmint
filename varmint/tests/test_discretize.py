64ba18901db831e16000d14b5ff8909aca4a3f8a
Ryan Adams <ryan.p.adams@gmail.com>
2020-12-22 13:30:19 -0500

    Have a fairly simple test failing due to the custom_jvp problem. Hopefully will give debugging insights.

--- varmint/tests/test_discretize.py
+++ varmint/tests/test_discretize.py
@@ -65,3 +65,71 @@ class Test_Discretize_Hamiltonian(ut.TestCase):
 
     self.assertAlmostEqual(q, 0.0)
     self.assertAlmostEqual(p, 0.0)
+
+
+  def test_stepping_pendulum_swing(self):
+
+    def lagrangian(q, qdot):
+      # Simple pendulum with a mass and length.
+      # q is angle from hanging down vertically.
+      gravity = 9.81 # m/s^2
+      mass    = 2.0   # kg
+      length  = 1.0   # m
+
+      # Needs scalars.
+      ke = np.sum(0.5 * mass * length**2 * qdot**2)
+      pe = -np.sum(mass*gravity*length*np.cos(q))
+
+      return ke - pe
+
+    stepper = get_hamiltonian_stepper(lagrangian)
+
+    dt = 0.01
+
+    q = np.ones(1) * np.pi * 10.0 / 180.0
+    p = np.zeros(1)
+
+    Q = []
+    P = []
+
+    # Roll forward two seconds. Very close to one period.
+    for ii in range(200):
+      q, p = stepper(q, p, dt)
+      Q.append(q)
+      P.append(p)
+
+    self.assertAlmostEqual(Q[0], Q[-1])
+
+
+  def test_stepping_pendulum_swing_grad(self):
+
+    def two_second_theta(params):
+      length = params[0]
+      mass   = params[1]
+      def lagrangian(q, qdot):
+        gravity = 9.81
+        ke = np.sum(0.5 * mass * length**2 * qdot**2)
+        pe = -np.sum(mass*gravity*length*np.cos(q))
+        return ke - pe
+
+      stepper = get_hamiltonian_stepper(lagrangian)
+
+      dt = 0.01
+
+      q = np.ones(1) * np.pi * 10.0 / 180.0
+      p = np.zeros(1)
+
+      Q = []
+      P = []
+
+      # Roll forward two seconds. Very close to one period.
+      for ii in range(200):
+        q, p = stepper(q, p, dt)
+        Q.append(q)
+        P.append(p)
+
+      return Q[-1]
+
+    g_2s_theta = jax.grad(two_second_theta)
+
+    print(g_2s_theta(np.array([1.0, 2.0])))
